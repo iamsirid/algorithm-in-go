58d8877d302b4e17674a1c976c3fda4161a899dc
iamsirid <iamsirid@gmail.com>
2023-02-27 08:24:06 +0700

    update merge sort algorithm

--- sorting/merge_sort.go
+++ sorting/merge_sort.go
@@ -4,7 +4,6 @@ func MergeSort(arr []float32) []float32 {
 	if len(arr) <= 1 {
 		return arr
 	}
-
 	mid := len(arr) / 2
 	left := MergeSort(arr[:mid])
 	right := MergeSort(arr[mid:])
@@ -13,20 +12,33 @@ func MergeSort(arr []float32) []float32 {
 }
 
 func merge(left, right []float32) []float32 {
-	var result []float32
+	result := make([]float32, len(left)+len(right))
 
-	for len(left) > 0 && len(right) > 0 {
-		if left[0] < right[0] {
-			result = append(result, left[0])
-			left = left[1:]
+	i, j, k := 0, 0, 0
+
+	for i < len(left) && j < len(right) {
+		if left[i] <= right[j] {
+			result[k] = left[i]
+			i++
 		} else {
-			result = append(result, right[0])
-			right = right[1:]
+			result[k] = right[j]
+			j++
 		}
+		k++
 	}
 
-	result = append(result, left...)
-	result = append(result, right...)
+	for i < len(left) {
+		result[k] = left[i]
+		i++
+		k++
+	}
+
+	for j < len(right) {
+		result[k] = right[j]
+		j++
+		k++
+	}
 
 	return result
+
 }
